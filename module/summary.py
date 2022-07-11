6f2c59e4ab09709858c68d89894f569e4aaf81bb
Jia Ming Low <66296475+Jming9638@users.noreply.github.com>
2022-07-11 12:57:53 +0800

    Update summary.py

--- module/summary.py
+++ module/summary.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def summary_res(total, result):
-    t_col = st.columns([0.2,1])
+    t_col = st.columns([0.25,1])
     with t_col[0]:
         st.header("Summary:")
     with t_col[1]:
@@ -9,7 +9,7 @@ def summary_res(total, result):
         st.write("")
         st.markdown("Total spend: **RM {:.2f}**".format(total))
     st.write('')
-    col = st.columns([0.3, 0.6, 0.3, 0.6, 0.5, 1.5])
+    col = st.columns([0.35, 0.65, 0.35, 0.65, 0.5, 1.5])
     for i in result.columns:
         for j in result.index:
             amount = result.loc[j,i]
@@ -17,10 +17,10 @@ def summary_res(total, result):
                 with col[0]:
                     st.write(i)
                 with col[1]:
-                    st.write("="*20)
+                    st.write("="*15)
                 with col[2]:
                     st.write("RM {:.2f}".format(amount))
                 with col[3]:
-                    st.write("="*19+">")
+                    st.write("="*14+">")
                 with col[4]:
                     st.write(j)
